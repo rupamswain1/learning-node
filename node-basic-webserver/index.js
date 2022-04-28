2174d417307e72477656f958cf52555067ef8307
rupamswain1 <rupamswain1@gmail.com>
2022-04-29 00:09:28 +0530

    basic node server done

--- node-basic-webserver/index.js
+++ node-basic-webserver/index.js
@@ -18,45 +18,59 @@ const pets = [
 ]
 
 const server = http.createServer((req, res) => {
+  const method = req.method
+
   const params = req.url.split('/')
-  if (params.length <= 2) {
-    if (params[1] === 'name') {
-      res.statusCode = 200
-      res.setHeader('Content-Type', 'application/json')
-      res.end(
-        JSON.stringify({
-          name: 'rupam',
-        }),
-      )
-    } else if (params[1] === 'html') {
-      res.statusCode = 200
-      res.setHeader('Content-Type', 'text/html')
-      res.write(`
-            <html>
-                <body>
-                    <h1>Hello HTML!!</h1>
-                </body>
-            </html>
-        `)
-      res.end()
-    } else if (params[1] === 'pets') {
-      res.statusCode = 200
-      res.setHeader('Content-Type', 'application/json')
-      res.end(JSON.stringify(pets))
-    } else {
-      res.statusCode = 404
-      res.end()
-    }
-  } else if (params.length === 3) {
-    if (params[1] === 'pets') {
-      if (params[2] <= pets.length - 1) {
+  if (method === 'GET') {
+    if (params.length <= 2) {
+      if (params[1] === 'name') {
         res.statusCode = 200
         res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify(pets[params[2]]))
+        res.end(
+          JSON.stringify({
+            name: 'rupam',
+          }),
+        )
+      } else if (params[1] === 'html') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        res.write(`
+              <html>
+                  <body>
+                      <h1>Hello HTML!!</h1>
+                  </body>
+              </html>
+          `)
+        res.end()
+      } else if (params[1] === 'pets') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify(pets))
       } else {
         res.statusCode = 404
         res.end()
       }
+    } else if (params.length === 3) {
+      if (params[1] === 'pets') {
+        if (params[2] <= pets.length - 1) {
+          res.statusCode = 200
+          res.setHeader('Content-Type', 'application/json')
+          res.end(JSON.stringify(pets[params[2]]))
+        } else {
+          res.statusCode = 404
+          res.end()
+        }
+      }
+    }
+  } else if (method === 'POST') {
+    console.log('this is post')
+    if (params[1] === 'pets') {
+      req.on('data', (data) => {
+        const { animal } = JSON.parse(data.toString())
+
+        pets.push({ id: pets.length, animal })
+        res.end(JSON.stringify(pets))
+      })
     }
   }
 })
