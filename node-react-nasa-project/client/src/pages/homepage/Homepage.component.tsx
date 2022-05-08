3b7b3a934306341d3e455745d8bb59e9be3e28e9
rupamswain1 <rupamswain1@gmail.com>
2022-05-09 01:00:38 +0530

    added homepage

--- node-react-nasa-project/client/src/pages/homepage/Homepage.component.tsx
+++ node-react-nasa-project/client/src/pages/homepage/Homepage.component.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios';
 import './Homepage.style.scss'
+interface LaunchData {
+  launchDate: string | null;
+  missionName: string | null;
+  rocketType: string | null;
 
+  destination: string | null;
+
+}
 function Homepage() {
+
+  const [launchData, setlaunchData] = useState<LaunchData>({ launchDate: null, missionName: null, rocketType: null, destination: null });
+  const [blankField, setBlankField] = useState<string[]>([]);
+  const [exoPlanets, setExoPlanets] = useState<string[]>([]);
+
+  const handleChange = (event: any): void => {
+
+    setlaunchData(((prevState) => ({ ...prevState, [event.target.name]: event.target.value })))
+
+  }
+  const handleSubmit = (event: any): void => {
+    event.preventDefault();
+    const emptyField: string[] = []
+    Object.keys(launchData).forEach((key: string) => {
+      const value = launchData[key as keyof LaunchData];
+      if (value !== null) {
+        console.log(true);
+      }
+      else {
+        emptyField.push(key)
+      }
+    })
+    setBlankField(emptyField)
+    console.log(blankField)
+  }
+  const getPlanets = () => {
+    axios.get('http://localhost:8000/planets')
+      .then(res => console.log(res.data))
+  }
   return <div className="homepage-container">
     <div className='missionLauncher'>
       <div>
@@ -19,23 +55,31 @@ function Homepage() {
           Effective Seller flux &gt; 0.36 times Earth's value and &lt; 1.11 times Earth's value
         </li>
       </ul>
-      <form className="launchInput">
+      {
+        blankField.length > 0 ?
+          <div className='launch-error'>
+            {`${blankField} are mandatory`}
+          </div>
+          : ''
+      }
+
+      <form className="launchInput" onSubmit={handleSubmit} >
         <div className='inputdata'>
           <label htmlFor="launchData">Launch Date</label>
-          <input type="date" id="launchData" />
+          <input type="date" id="launchData" onChange={handleChange} name="launchDate" />
         </div>
         <div className='inputdata'>
           <label htmlFor="missionName">Mission Name</label>
-          <input type="text" id="missionName" />
+          <input type="text" id="missionName" name="missionName" onChange={handleChange} />
         </div>
         <div className='inputdata'>
           <label htmlFor="rocketType">Rocket type</label>
-          <input type="text" id="rocketType" />
+          <input type="text" id="rocketType" name="rocketType" onChange={handleChange} />
         </div>
         <div className='inputdata'>
           <label htmlFor="destExoPlanet">Destination Exoplanet</label>
-          <select id="destExoPlanet">
-            <option value="test">Test</option>
+          <select id="destExoPlanet" name="destination" onChange={handleChange} onFocus={getPlanets}>
+            <option value=" "> </option>
           </select>
         </div>
         <input className="launchBtn" type="submit" value="Launch Mission" />
