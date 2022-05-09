48745935a053b10d93596ff631066823163f4aa9
rupamswain1 <rupamswain1@gmail.com>
2022-05-10 00:57:49 +0530

    cors added

--- node-react-nasa-project/server/build/app.js
+++ node-react-nasa-project/server/build/app.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const planets_route_1 = __importDefault(require("./routes/planets/planets.route"));
 const app = express_1.default();
+app.use(cors_1.default());
 app.use(express_1.default.json());
 app.use(planets_route_1.default);
 exports.default = app;
--- node-react-nasa-project/server/src/app.ts
+++ node-react-nasa-project/server/src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express'
+import cors from 'cors'
 import getPlanetRouter from './routes/planets/planets.route'
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.use(getPlanetRouter)
 
