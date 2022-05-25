7079d53b71c5a2b4da0445e295520265128fd206
rupamswain1 <rupamswain1@gmail.com>
2022-05-26 01:12:56 +0530

    adding the launch model

---
+++ node-react-nasa-project/server/src/models/launches.model.ts
@@ -0,0 +1,15 @@
+type launchInterface{
+    flightNumber:Number,
+    mission:String,
+    rocket:String,
+    launchDate:Date,
+    destination:String,
+    customer:String[],
+    upcoming:boolean,
+    success:boolean
+}
+
+const launches:launchInterface[]=[
+    
+]
+
