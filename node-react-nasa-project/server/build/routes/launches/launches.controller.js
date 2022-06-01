125aaa33a7d8cd3b2082379d2974512ac623ea05
rupamswain1 <rupamswain1@gmail.com>
2022-06-02 01:13:18 +0530

    added validation to launch api

--- node-react-nasa-project/server/build/routes/launches/launches.controller.js
+++ node-react-nasa-project/server/build/routes/launches/launches.controller.js
@@ -8,7 +8,17 @@ const httpGetAllLaunches = (req, res) => {
 exports.httpGetAllLaunches = httpGetAllLaunches;
 const httpAddLaunch = (req, res) => {
     const launch = req.body;
+    if (!launch.misson ||
+        !launch.rocket ||
+        !launch.launchDate ||
+        !launch.destination ||
+        !launch.customer) {
+        return res.status(400).json({ error: 'invalid launch property' });
+    }
     launch.launchDate = new Date(launch.launchDate);
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({ error: 'invalid launch date' });
+    }
     launches_model_1.addNewLaunch(launch);
     return res.status(201).json(launches_model_1.getAllLaunches());
 };
--- node-react-nasa-project/server/src/routes/launches/launches.controller.ts
+++ node-react-nasa-project/server/src/routes/launches/launches.controller.ts
@@ -8,7 +8,21 @@ export const httpGetAllLaunches = (req: Request, res: Response) => {
 
 export const httpAddLaunch = (req: Request, res: Response) => {
   const launch = req.body
+
+  if (
+    !launch.misson ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination ||
+    !launch.customer
+  ) {
+    return res.status(400).json({ error: 'invalid launch property' })
+  }
+
   launch.launchDate = new Date(launch.launchDate)
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({ error: 'invalid launch date' })
+  }
   addNewLaunch(launch)
   return res.status(201).json(getAllLaunches())
 }
