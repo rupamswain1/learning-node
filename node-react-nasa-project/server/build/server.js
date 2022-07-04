b7934aacc7e810ae1a45e1595fc9e8cfc8841fe2
rupamswain1 <rupamswain1@gmail.com>
2022-07-05 00:26:14 +0530

    added mongoose and connection eshtablished

--- node-react-nasa-project/server/build/server.js
+++ node-react-nasa-project/server/build/server.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -13,11 +36,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv = __importStar(require("dotenv"));
 const app_1 = __importDefault(require("./app"));
 const planets_model_1 = require("./models/planets.model");
+dotenv.config();
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = process.env.mongo_url || 'dummy url';
+mongoose_1.default.connection.once('open', () => {
+    console.log('MongoDB connection Eshtablished');
+});
+mongoose_1.default.connection.on('error', () => {
+    console.log('mongoDB connection failed');
+});
 const server = http_1.default.createServer(app_1.default);
 const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield mongoose_1.default.connect(MONGO_URL);
     yield (0, planets_model_1.loadPlanets)();
     server.listen(PORT, () => {
         console.log(`Listining on port ${PORT}`);
--- node-react-nasa-project/server/src/server.ts
+++ node-react-nasa-project/server/src/server.ts
@@ -1,11 +1,27 @@
 import http from 'http'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+
 import app from './app'
 import { loadPlanets } from './models/planets.model'
+
+dotenv.config()
+
 const PORT = process.env.PORT || 8000
+const MONGO_URL = process.env.mongo_url || 'dummy url'
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection Eshtablished')
+})
+
+mongoose.connection.on('error', () => {
+  console.log('mongoDB connection failed')
+})
 
 const server = http.createServer(app)
 
 const startServer = async () => {
+  await mongoose.connect(MONGO_URL)
   await loadPlanets()
   server.listen(PORT, () => {
     console.log(`Listining on port ${PORT}`)
