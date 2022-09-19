444be1b63c60da513998d07e6665d483c5b2198f
rupamswain1 <rupamswain1@gmail.com>
2022-09-20 00:14:44 +0530

    product schema added

--- node-graphql/src/server.js
+++ node-graphql/src/server.js
@@ -3,15 +3,74 @@ const { buildSchema } = require('graphql')
 const { graphqlHttp, graphqlHTTP } = require('express-graphql')
 
 const schema = buildSchema(`
-    type Query{
-        description: String,
-        price:Float
-    }
+   type Query{
+    products:[Product]
+    orders:[Order]
+   }
+
+   type Product{
+    id:ID!
+    description:String!
+    reviews:[Review]
+    price:Float!
+   }
+
+   type Review{
+    rating: Int!
+    comment:String
+   }
+
+   type Order{
+    date:String!
+    subTotal:Float!
+    items:[OrderItems]
+   }
+
+   type OrderItems{
+    product:Product!
+    quantity:Int!
+   }
 `)
 
 const root = {
-  description: 'Red Shoe',
-  price: 42.12,
+  products: [
+    {
+      id: '3060gc',
+      description: '3060 graphic card',
+      price: 35000,
+      reviews: [
+        {
+          rating: 5,
+          comment: 'good',
+        },
+        {
+          rating: 2,
+          comment: 'broken item recieved',
+        },
+      ],
+    },
+    {
+      id: 'inti5',
+      description: 'Intel i5',
+      price: 36000,
+    },
+  ],
+  orders: [
+    {
+      date: '01-01-2022',
+      subTotal: 700000.0,
+      items: [
+        {
+          product: {
+            id: '3060gc',
+            description: 'Asus 3060 gc',
+            price: 80000,
+          },
+          quantity: 1,
+        },
+      ],
+    },
+  ],
 }
 
 const app = express()
@@ -21,6 +80,7 @@ app.use(
   graphqlHTTP({
     schema: schema,
     rootValue: root,
+    graphiql: true,
   }),
 )
 
